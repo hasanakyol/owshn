0783f02bcceb1767592f0bd37d5cb13787ac8380
Hasan Akyol <hasanakyol@gmail.com>
2021-03-26 20:45:02 +0000

    test 3

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,7 @@ export default {
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Owshn</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        an Avalanche Ecosystem 
-      </span>
+      <span className="text-gray-600 font-normal hidden md:inline">an Avalanche Ecosystem</span>
     </>
   ),
   head: (
@@ -15,15 +13,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="Owshn an Avalanche Ecosystem" />
+      <meta name="og:description" content="Owshn an Avalanche Ecosystem" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://owshn.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="owshn.vercel.app" />
+      <meta name="twitter:url" content="https://owshn.vercel.app" />
+      <meta name="og:title" content="Owshn an Avalanche Ecosystem" />
+      <meta name="og:image" content="https://owshn.vercel.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Owshn" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
@@ -42,6 +40,4 @@ export default {
   footer: true,
   footerEditOnGitHubLink: true,
   footerText: <>MIT {new Date().getFullYear()} © Owshn.</>
-
- 
 }
